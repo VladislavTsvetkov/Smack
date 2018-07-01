5450b131fd11483e9cbcc2cb7bcfec564e67e6c4
Владислав Цветков <vladislavcvetkov@MBP-Vladislav.lan>
2018-07-02 00:16:42 +0300

    Login Screen

---
+++ Smack/Utillities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Владислав Цветков on 01.07.2018.
+//  Copyright © 2018 Владислав Цветков. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
