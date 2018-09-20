fd7815b3f1f758b50c4400b52c9f5dade6491a2f
Владислав Цветков <>
2018-09-21 02:13:58 +0300

    now app can get channels from db

---
+++ Smack/Services/Channel.swift
@@ -0,0 +1,17 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Владислав Цветков on 21/09/2018.
+//  Copyright © 2018 Владислав Цветков. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    var channelTitle: String!
+    var channelDescription: String!
+    var id: String
+    
+}
---
+++ Smack/Services/MessageService.swift
@@ -0,0 +1,42 @@
+//
+//  MessageService.swift
+//  Smack
+//
+//  Created by Владислав Цветков on 21/09/2018.
+//  Copyright © 2018 Владислав Цветков. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannel(completion: @escaping CompletionHandler) {
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            if response.result.error == nil {
+                
+                guard let data = response.data else {return}
+                if let json = try? JSON(data: data).array { // how to make it correctly ?
+                    guard let unwrappedJSON = json else {return}
+                    for item in unwrappedJSON {
+                        let name = item["name"].stringValue
+                        let channelDescription = item["description"].stringValue
+                        let id = item["_id"].stringValue
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                        self.channels.append(channel)
+                    }
+                    print(self.channels) // p
+                    completion(true)
+                }
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+}
